7b5c4f84f7dbfb9d165d0643110069b5ba497e03
GLOBAL\v747369 <suraj.dhopati@united.com>
2025-02-11 18:43:01 -0500

    Initial Commit

---
+++ RickAndMortySearchApp/RickAndMortySearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortySearchAppApp.swift
+//  RickAndMortySearchApp
+//
+//  Created by Suraj Raju Dhopati on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortySearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RickAndMortySearchAppTests/RickAndMortySearchAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortySearchAppTests.swift
+//  RickAndMortySearchAppTests
+//
+//  Created by Suraj Raju Dhopati on 2/11/25.
+//
+
+import Testing
+@testable import RickAndMortySearchApp
+
+struct RickAndMortySearchAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
